93dbb700c0531b22284f4e60ee0849fc7ab6aa9e
YooHyuk Lee <azsxd34@naver.com>
2020-06-24 21:04:50 +0900

    Initial commit

---
+++ src/main/java/com/practice/reason/ReasonApplication.java
@@ -0,0 +1,13 @@
+package com.practice.reason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReasonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReasonApplication.class, args);
+    }
+
+}
